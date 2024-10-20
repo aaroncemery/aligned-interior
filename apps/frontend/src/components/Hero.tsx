01b622a7d4a72f2df8856b64b80ed03781ac4c75
Aaron Emery <aemery81@gmail.com>
2024-10-20 10:59:34 -0700

    chore: hero refactor

--- apps/frontend/src/components/Hero.tsx
+++ apps/frontend/src/components/Hero.tsx
@@ -9,18 +9,14 @@ interface HeroProps {
 const Hero = ({ backgroundImage, title, subtitle }: HeroProps) => {
   return (
     <div
-      className='relative h-screen flex items-center justify-center'
-      style={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
+      className='relative h-screen flex items-center justify-center bg-cover bg-center'
+      style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className='absolute inset-0 bg-black opacity-50'></div>
       <div className='relative text-center text-white z-10'>
         <h1 className='text-4xl md:text-6xl font-cinzel mb-4'>{title}</h1>
         {subtitle && (
-          <p className='text-xl font-belleAurore md:text-2xl'>{subtitle}</p>
+          <p className='text-xl md:text-2xl font-belleAurore'>{subtitle}</p>
         )}
       </div>
     </div>
