a3fb55f77b1286d1c7282f20c4b7c07a1be867ff
Aaron Emery <aaron.emery@sanity.io>
2024-12-16 18:29:56 -0800

    feat: testimonial header

--- apps/frontend/src/components/testimonial/TestimonialBlock.tsx
+++ apps/frontend/src/components/testimonial/TestimonialBlock.tsx
@@ -2,15 +2,15 @@ import TestimonialCard from "./TestimonialCard";
 
 export default function TestimonialBlock() {
   return (
-    <>
-      <div className="flex justify-center">
+    <div className="container">
+      <div className="my-10 flex flex-[1_0_0] flex-col items-center justify-center lg:my-28">
         <h2 className="font-cinzel text-6xl">Testimonials</h2>
       </div>
-      <div className="container flex items-center justify-center gap-4">
+      <div className="flex items-center justify-center gap-4">
         <TestimonialCard />
         <TestimonialCard />
         <TestimonialCard />
       </div>
-    </>
+    </div>
   );
 }
