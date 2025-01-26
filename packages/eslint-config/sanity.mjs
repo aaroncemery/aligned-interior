02a8af9f0faa4ea9c42ec2c4019273c0172e4a57
Aaron Emery <aemery81@gmail.com>
2025-01-26 09:12:52 -0800

    chore: updatine sanity eslint config

--- packages/eslint-config/sanity.mjs
+++ packages/eslint-config/sanity.mjs
@@ -1,44 +1,39 @@
-import { fileURLToPath } from 'node:url';
-import { dirname, resolve } from 'node:path';
-import sanityStudioConfig from '@sanity/eslint-config-studio';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import sanityStudio from '@sanity/eslint-config-studio';
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
+import tseslint from 'typescript-eslint';
 
 const project = resolve(process.cwd(), 'tsconfig.json');
 
-/** @type {import("eslint").Linter.Config} */
-export default {
-  extends: [sanityStudioConfig, 'turbo'],
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  env: {
-    node: true,
-    browser: true,
-  },
-  plugins: ['prettier', 'simple-import-sort', 'eslint-plugin-import'],
-  settings: {
-    'import/resolver': {
-      typescript: {
-        project,
+/** @type {import('eslint').Linter.Config} */
+export default [
+  ...sanityStudio,
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+      'simple-import-sort': eslintPluginSimpleImportSort,
+      import: eslintPluginImport,
+      '@typescript-eslint': tseslint.plugin,
+    },
+    languageOptions: {
+      globals: {
+        React: true,
+        JSX: true,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
     },
+    ignores: ['dist', 'node_modules', '.sanity', '.*.js'],
+    rules: {
+      'prettier/prettier': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'import/no-default-export': 'error',
+    },
   },
-  ignorePatterns: [
-    // Ignore dotfiles
-    'dist',
-    '.*.js',
-    'node_modules/',
-    '.sanity',
-  ],
-  rules: {
-    'prettier/prettier': 'error',
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
-    '@typescript-eslint/consistent-type-imports': 'error',
-    'import/no-default-export': 'error',
-  },
-  overrides: [{ files: ['*.js?(x)', '*.ts?(x)'] }],
-};
+];
