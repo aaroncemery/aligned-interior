73c3bb0bd95c8bb4d1f101a9144306dd18d9d2a7
Aaron Emery <aaron.emery@sanity.io>
2025-04-10 17:37:54 -0700

    feat: adding navigation to blog

--- apps/frontend/src/app/blog/layout.tsx
+++ apps/frontend/src/app/blog/layout.tsx
@@ -3,6 +3,7 @@ import { Footer } from "@/components/ui/Footer";
 import { SeoQuery, NavigationQuery } from "@/sanity/lib/queries";
 import { sanityFetch } from "@/sanity/lib/fetch";
 import { SeoQueryResult } from "../../../sanity.types";
+import { NavigationProvider } from "@/components/ui/nav/NavigationProvider";
 
 export async function generateMetadata() {
   const settings = await sanityFetch<SeoQueryResult>({ query: SeoQuery });
@@ -19,16 +20,33 @@ export async function generateMetadata() {
   };
 }
 
-export default function RootLayout({
+interface NavigationData
+  extends Array<{
+    items: Array<{
+      label: string;
+      url: string;
+    }>;
+  }> {}
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const navigationData = await sanityFetch<NavigationData>({
+    query: NavigationQuery,
+  });
+
+  const navItems =
+    navigationData[0]?.items?.map((item) => ({
+      label: item.label,
+      href: item.url,
+      isSection: item.url.startsWith("#"),
+    })) || [];
+
   return (
     <>
-      <Navigation.Desktop isStatic={true} />
-      {children}
-      <Footer />
+      <NavigationProvider items={navItems}>{children}</NavigationProvider>
     </>
   );
 }
