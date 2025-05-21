f20e668c86fb922ec939c7520718fb701317cbbb
Aaron Emery <aaron.emery@sanity.io>
2025-05-21 15:54:17 -0700

    chore: removing unused import

--- apps/frontend/src/app/blog/[slug]/page.tsx
+++ apps/frontend/src/app/blog/[slug]/page.tsx
@@ -2,7 +2,6 @@ import { sanityFetch } from "@/sanity/lib/live";
 import { BlogPostQuery } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
 import BlogPage from "@/components/Pages/BlogPage";
-import { BlogPostQueryResult } from "../../../../sanity.types";
 import { Suspense } from "react";
 import BlogPageSkeleton from "@/components/Pages/BlogPageSkeleton";
 import { SanityLive } from "@/sanity/lib/live";
